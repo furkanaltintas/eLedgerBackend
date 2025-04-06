10dda4b8dde382a5ffd9c58d4d63198ad6d5a767
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-07 02:41:03 +0300

    di

---
+++ Persistence/DependencyInjection.cs
@@ -0,0 +1,61 @@
+﻿using Application.Services;
+using Domain.Entities;
+using Domain.Interfaces;
+using GenericRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Context;
+using Persistence.Repositories;
+using Persistence.Services;
+
+namespace Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<CompanyDbContext>();
+
+        services.AddDbContext<AppDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddIdentity<AppUser, IdentityRole<Guid>>(options =>
+        {
+            options.Password.RequireDigit = false;
+            options.Password.RequiredLength = 3;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireLowercase = false;
+        })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+
+        Repository(services);
+
+        services.AddScoped<IUnitOfWork>(srv => srv.GetRequiredService<AppDbContext>());
+        services.AddScoped<IUnitOfWorkCompany>(srv => srv.GetRequiredService<CompanyDbContext>());
+
+        services.AddScoped<ICompanyService, CompanyService>();
+
+        return services;
+    }
+
+    private static void Repository(IServiceCollection services)
+    {
+        services.AddScoped<IBankDetailRepository, BankDetailRepository>();
+        services.AddScoped<IBankRepository, BankRepository>();
+        services.AddScoped<ICashRegisterDetailRepository, CashRegisterDetailRepository>();
+        services.AddScoped<ICompanyRepository, CompanyRepository>();
+        services.AddScoped<ICompanyUserRepository, CompanyUserRepository>();
+        services.AddScoped<ICustomerDetailRepository, CustomerDetailRepository>();
+        services.AddScoped<ICustomerRepository, CustomerRepository>();
+        services.AddScoped<IInvoiceDetailRepository, InvoiceDetailRepository>();
+        services.AddScoped<IInvoiceRepository, InvoiceRepository>();
+        services.AddScoped<IProductDetailRepository, ProductDetailRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+    }
+}
