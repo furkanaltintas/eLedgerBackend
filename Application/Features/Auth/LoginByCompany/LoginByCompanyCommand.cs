92022ff03d5132d9123b78fdc5ffca11ff801c6d
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-07 02:42:03 +0300

    login-by-company

---
+++ Application/Features/Auth/LoginByCompany/LoginByCompanyCommand.cs
@@ -0,0 +1,7 @@
+﻿using DomainResults.Common;
+using Infrastructure.Configurations;
+using MediatR;
+
+namespace Application.Features.Auth.LoginByCompany;
+
+public record LoginByCompanyCommand(string UserId, string CompanyId) : IRequest<IDomainResult<LoginCommandResponse>>;
---
+++ Application/Features/Auth/LoginByCompany/LoginByCompanyCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using DomainResults.Common;
+using Infrastructure.Configurations;
+using Infrastructure.Services;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Auth.LoginByCompany;
+
+class LoginByCompanyCommandHandler(
+    UserManager<AppUser> userManager,
+    ICompanyUserRepository companyUserRepository,
+    IJwtTokenGenerator jwtTokenGenerator) : IRequestHandler<LoginByCompanyCommand, IDomainResult<LoginCommandResponse>>
+{
+    public async Task<IDomainResult<LoginCommandResponse>> Handle(LoginByCompanyCommand request, CancellationToken cancellationToken)
+    {
+        AppUser? user = await userManager.FindByIdAsync(request.UserId);
+        if(user is null) return DomainResult<LoginCommandResponse>.NotFound("User not found");
+
+        List<Company> companies = await companyUserRepository
+            .Where(c => c.AppUserId == Guid.Parse(request.UserId))
+            .Include(c => c.Company)
+            .Select(c => new Company
+            {
+                Id = c.CompanyId,
+                Name = c.Company!.Name,
+                TaxDepartment = c.Company.TaxDepartment,
+                TaxNumber = c.Company.TaxNumber,
+                FullAddress = c.Company.FullAddress
+            })
+            .OrderBy(c => c.Name)
+            .ToListAsync(cancellationToken);
+
+        LoginCommandResponse loginResponse = await jwtTokenGenerator.CreateToken(user, Guid.Parse(request.CompanyId), companies);
+        return DomainResult.Success(loginResponse);
+    }
+}
