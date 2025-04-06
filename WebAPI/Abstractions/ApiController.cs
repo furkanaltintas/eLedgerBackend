823da3bf8050900d01e526ad85a6ec63525598ec
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-07 02:43:49 +0300

    api

---
+++ WebAPI/Abstractions/ApiController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using DomainResults.Common;
+using MediatR;
+
+namespace WebAPI.Abstractions;
+
+[Route("api/[controller]")]
+[Authorize(AuthenticationSchemes = "Bearer")]
+[ApiController]
+public class ApiController(IMediator mediator) : ControllerBase
+{
+    protected IMediator Mediator => mediator;
+
+    // Genel Çevirici
+    public async Task<IActionResult> Send<TRequest, TResponse>(TRequest request, CancellationToken cancellationToken = default)
+        where TRequest : IRequest<TResponse>
+    {
+        var response = await mediator.Send(request, cancellationToken);
+        return this.Response(response);
+    }
+
+    // Result yapımız
+    public async Task<IActionResult> Send<TResponse>(IRequest<IDomainResult<TResponse>> request, CancellationToken cancellationToken = default)
+    {
+        var response = await mediator.Send(request, cancellationToken);
+        return this.DomResponse(response);
+    }
+
+    // No Result
+    public async Task<IActionResult> NoSend<TRequest>(TRequest request, CancellationToken cancellationToken = default)
+        where TRequest : IRequest
+    {
+        await mediator.Send(request, cancellationToken);
+        return this.NoResponse<TRequest>();
+    }
+}
---
+++ WebAPI/Abstractions/ApiResponse.cs
@@ -0,0 +1,24 @@
+﻿using DomainResults.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Abstractions;
+
+public static class ApiResponse
+{
+    public static IActionResult Response<T>(this ControllerBase controller, T result)
+    {
+        if (result is not null) return controller.Ok(result);
+        return controller.NotFound(result);
+    }
+
+    public static IActionResult NoResponse<T>(this ControllerBase controller)
+    {
+        return controller.NoContent();
+    }
+
+    public static IActionResult DomResponse<T>(this ControllerBase controller, IDomainResult<T> result)
+    {
+        if (result.IsSuccess) return controller.Ok(result);
+        return controller.NotFound(result);
+    }
+}
