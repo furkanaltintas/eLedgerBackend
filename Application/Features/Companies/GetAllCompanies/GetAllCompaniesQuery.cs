faf465a22f4f69e735d92c77e764fc6ab40f9bf8
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-07 02:43:05 +0300

    get-all-companies

---
+++ Application/Features/Companies/GetAllCompanies/GetAllCompaniesQuery.cs
@@ -0,0 +1,7 @@
+﻿using Domain.Entities;
+using DomainResults.Common;
+using MediatR;
+
+namespace Application.Features.Companies.GetAllCompanies;
+
+public record GetAllCompaniesQuery(): IRequest<IDomainResult<List<Company>>>;
---
+++ Application/Features/Companies/GetAllCompanies/GetAllCompaniesQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using DomainResults.Common;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Companies.GetAllCompanies;
+
+class GetAllCompaniesQueryHandler(ICompanyRepository companyRepository) : IRequestHandler<GetAllCompaniesQuery, IDomainResult<List<Company>>>
+{
+    public async Task<IDomainResult<List<Company>>> Handle(GetAllCompaniesQuery request, CancellationToken cancellationToken)
+    {
+        List<Company> companies = await companyRepository
+            .GetAll()
+            .OrderBy(c => c.Name)
+            .ToListAsync(cancellationToken);
+        return DomainResult.Success(companies);
+    }
+}
