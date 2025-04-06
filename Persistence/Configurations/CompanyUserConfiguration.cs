e7c6e4f01a32290a36bbff9598874e5113d92012
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-07 02:40:19 +0300

    Configurations

---
+++ Persistence/Configurations/CompanyUserConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Configurations;
+
+class CompanyUserConfiguration : IEntityTypeConfiguration<CompanyUser>
+{
+    public void Configure(EntityTypeBuilder<CompanyUser> builder)
+    {
+        builder.HasKey(x => new { x.AppUserId, x.CompanyId });
+        builder.HasQueryFilter(filter => !filter.Company!.IsDeleted);
+    }
+}
