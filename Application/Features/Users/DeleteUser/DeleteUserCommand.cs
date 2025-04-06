ebdf513de010e97faa4f97cacd7f19659c20987a
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-07 02:42:25 +0300

    delete-user

---
+++ Application/Features/Users/DeleteUser/DeleteUserCommand.cs
@@ -0,0 +1,6 @@
+﻿using DomainResults.Common;
+using MediatR;
+
+namespace Application.Features.Users.DeleteUser;
+
+public record DeleteUserCommand(Guid Id) : IRequest<IDomainResult<string>>;
---
+++ Application/Features/Users/DeleteUser/DeleteUserCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using DomainResults.Common;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.Features.Users.DeleteUser;
+
+class DeleteUserCommandHandler(
+    UserManager<AppUser> userManager) : IRequestHandler<DeleteUserCommand, IDomainResult<string>>
+{
+    public async Task<IDomainResult<string>> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+    {
+        AppUser? user = await userManager.FindByIdAsync(request.Id.ToString());
+        if (user is null) return DomainResult<string>.NotFound("User not found");
+
+        user.IsDeleted = true;
+
+        IdentityResult identityResult = await userManager.UpdateAsync(user);
+        if (!identityResult.Succeeded) return DomainResult<string>.Failed(identityResult.Errors.Select(e => e.Description).ToList());
+
+        return DomainResult.Success("User deleted successfully");
+    }
+}
+
