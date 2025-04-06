425d5805a1515b29ac6d55ff68f8ce2d6d38ca96
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-07 02:43:42 +0300

    di

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Mapster;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        Assembly assembly = Assembly.GetExecutingAssembly();
+
+        services.AddMapster();
+
+        services.AddMediatR(configuration =>
+        { configuration.RegisterServicesFromAssemblies(assembly, typeof(AppUser).Assembly); });
+
+        return services;
+    }
+}
