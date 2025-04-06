dbe00be9f72c76976f8dbc690d9814a1e62e252b
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-07 02:43:24 +0300

    update-company

---
+++ Application/Features/Companies/UpdateCompany/UpdateCompanyCommand.cs
@@ -0,0 +1,13 @@
+﻿using Domain.ValueObjects;
+using DomainResults.Common;
+using MediatR;
+
+namespace Application.Features.Companies.UpdateCompany;
+
+public record UpdateCompanyCommand(
+    Guid Id,
+    string Name,
+    string FullAddress,
+    string TaxDepartment,
+    string TaxNumber,
+    Database Database) : IRequest<IDomainResult<string>>;
---
+++ Application/Features/Companies/UpdateCompany/UpdateCompanyCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using DomainResults.Common;
+using GenericRepository;
+using MapsterMapper;
+using MediatR;
+
+namespace Application.Features.Companies.UpdateCompany;
+
+class UpdateCompanyCommandHandler(
+    ICompanyRepository companyRepository,
+    IUnitOfWork unitOfWork,
+    IMapper mapper) : IRequestHandler<UpdateCompanyCommand, IDomainResult<string>>
+{
+    public async Task<IDomainResult<string>> Handle(UpdateCompanyCommand request, CancellationToken cancellationToken)
+    {
+        Company company = await companyRepository.GetByExpressionWithTrackingAsync(c => c.Id == request.Id, cancellationToken);
+        if(company is null) return DomainResult.NotFound<string>("Şirket bulunamadı");
+
+        if(company.TaxNumber != request.TaxNumber)
+        {
+            Boolean isTaxNumberExists = await companyRepository.AnyAsync(c => c.TaxNumber == request.TaxNumber, cancellationToken);
+            if (isTaxNumberExists) return DomainResult.Conflict<string>("Bu vergi numarasına sahip başka bir şirket bulunmaktadır");
+        }
+
+        mapper.Map(request, company);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+        return DomainResult.Success("Şirket bilgisi başarıyla güncellendi");
+    }
+}
