3f7029d318bcdc481f36d2412c385268f520c8ab
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-07 02:40:58 +0300

    Services

---
+++ Persistence/Services/CompanyService.cs
@@ -0,0 +1,18 @@
+﻿using Application.Services;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Context;
+
+namespace Persistence.Services;
+
+class CompanyService : ICompanyService
+{
+    public void MigrateAllCompanies(List<Company> companies)
+    {
+        foreach (Company company in companies)
+        {
+            CompanyDbContext context = new(company);
+            context.Database.Migrate();
+        }
+    }
+}
