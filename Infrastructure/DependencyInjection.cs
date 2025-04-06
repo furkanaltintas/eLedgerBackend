8e17dddeaeb77545965bc72d5fa054659b3e5bd7
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-07 02:41:32 +0300

    di

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddMemoryCache();
+
+        return services;
+    }
+}
