25cbec353c5184a9fca80499a2c2d4b7c5bf030d
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-07 02:42:32 +0300

    get-all-user

---
+++ Application/Features/Users/GetAllUsers/GetAllUsersQuery.cs
@@ -0,0 +1,7 @@
+﻿using Domain.Entities;
+using DomainResults.Common;
+using MediatR;
+
+namespace Application.Features.Users.GetAllUsers;
+
+public record GetAllUsersQuery(): IRequest<IDomainResult<List<AppUser>>>;
---
+++ Application/Features/Users/GetAllUsers/GetAllUsersQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using DomainResults.Common;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Users.GetAllUsers;
+
+class GetAllUsersQueryHandler(UserManager<AppUser> userManager) : IRequestHandler<GetAllUsersQuery, IDomainResult<List<AppUser>>>
+{
+    public async Task<IDomainResult<List<AppUser>>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+    {
+        List<AppUser> users = await userManager.Users
+            .Include(u => u.CompanyUsers)!
+            .ThenInclude(cu => cu.Company)
+            .OrderBy(u => u.FirstName)
+            .ThenBy(u => u.LastName)
+            .ToListAsync(cancellationToken);
+
+        return DomainResult.Success(users);
+    }
+}
