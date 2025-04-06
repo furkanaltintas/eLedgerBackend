e1583cbed93f426e3e2403e47fc78674e9595014
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-07 02:42:49 +0300

    create-company

---
+++ Application/Features/Companies/CreateCompany/CreateCompanyCommand.cs
@@ -0,0 +1,12 @@
+﻿using Domain.ValueObjects;
+using DomainResults.Common;
+using MediatR;
+
+namespace Application.Features.Companies.CreateCompany;
+
+public record CreateCompanyCommand(
+    string Name,
+    string FullAddress,
+    string TaxDepartment,
+    string TaxNumber,
+    Database Database): IRequest<IDomainResult<string>>;
---
+++ Application/Features/Companies/CreateCompany/CreateCompanyCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using DomainResults.Common;
+using GenericRepository;
+using MapsterMapper;
+using MediatR;
+
+namespace Application.Features.Companies.CreateCompany;
+
+class CreateCompanyCommandHandler(
+    ICompanyRepository companyRepository,
+    IUnitOfWork unitOfWork,
+    IMapper mapper) : IRequestHandler<CreateCompanyCommand, IDomainResult<string>>
+{
+    public async Task<IDomainResult<string>> Handle(CreateCompanyCommand request, CancellationToken cancellationToken)
+    {
+        Boolean isTaxNumberExists = await companyRepository.AnyAsync(c => c.TaxNumber == request.TaxNumber, cancellationToken);
+        if (isTaxNumberExists) return DomainResult.Conflict<string>("Bu vergi numarasına sahip başka bir şirket bulunmaktadır");
+
+        Company company = mapper.Map<Company>(request);
+        await companyRepository.AddAsync(company, cancellationToken);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+        return DomainResult.Success("Şirket başarıyla oluşturuldu");
+    }
+}
