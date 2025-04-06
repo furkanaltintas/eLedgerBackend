e830a3d1e48579be1775b38b6644e9f934c24bef
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-07 02:43:32 +0300

    user-companies

---
+++ Application/Features/Companies/UserCompanies/UserCompaniesQuery.cs
@@ -0,0 +1,7 @@
+﻿using Domain.Entities;
+using DomainResults.Common;
+using MediatR;
+
+namespace Application.Features.Companies.UserCompanies;
+
+public record UserCompaniesQuery(string UserId) : IRequest<IDomainResult<List<Company>>>;
---
+++ Application/Features/Companies/UserCompanies/UserCompaniesQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using DomainResults.Common;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Companies.UserCompanies;
+
+class UserCompaniesQueryHandler(
+    ICompanyUserRepository companyUserRepository) : IRequestHandler<UserCompaniesQuery, IDomainResult<List<Company>>>
+{
+    public async Task<IDomainResult<List<Company>>> Handle(UserCompaniesQuery request, CancellationToken cancellationToken)
+    {
+        List<Company> companies = await companyUserRepository
+            .GetAll()
+            .Where(c => c.AppUserId == Guid.Parse(request.UserId))
+            .Include(c => c.Company)
+            .Select(c => new Company
+            {
+                Id = c.CompanyId,
+                Name = c.Company!.Name
+            })
+            .ToListAsync(cancellationToken);
+
+        return DomainResult.Success(companies);
+    }
+}
