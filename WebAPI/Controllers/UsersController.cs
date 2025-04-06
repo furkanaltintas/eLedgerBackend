a09d435fe81f45fca2ed880cf3dd0d7a2dc77d66
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-07 02:44:08 +0300

    users

---
+++ WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using Application.Features.Users.CreateUser;
+using Application.Features.Users.DeleteUser;
+using Application.Features.Users.GetAllUsers;
+using Application.Features.Users.UpdateUser;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Abstractions;
+
+namespace WebAPI.Controllers;
+
+public class UsersController : ApiController
+{
+    public UsersController(IMediator mediator) : base(mediator) { }
+
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll() =>
+        await Send(new GetAllUsersQuery());
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateUserCommand createUserCommand) =>
+        await Send(createUserCommand);
+
+    [HttpPut]
+    public async Task<IActionResult> Update(UpdateUserCommand updateUserCommand) =>
+        await Send(updateUserCommand);
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(DeleteUserCommand deleteUserCommand) =>
+        await Send(deleteUserCommand);
+}
