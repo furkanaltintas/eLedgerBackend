4e56d79e97aad9686305a27009004d7b6c336161
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-07 02:42:11 +0300

    change-company

---
+++ Application/Features/Auth/ChangeCompany/ChangeCompanyCommand.cs
@@ -0,0 +1,7 @@
+﻿using DomainResults.Common;
+using Infrastructure.Configurations;
+using MediatR;
+
+namespace Application.Features.Auth.ChangeCompany;
+
+public record ChangeCompanyCommand(Guid CompanyId) : IRequest<IDomainResult<LoginCommandResponse>>;
---
+++ Application/Features/Auth/ChangeCompany/ChangeCompanyCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using DomainResults.Common;
+using Infrastructure.Configurations;
+using Infrastructure.Services;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace Application.Features.Auth.ChangeCompany;
+
+class ChangeCompanyCommandHandler(
+    ICompanyUserRepository companyUserRepository,
+    UserManager<AppUser> userManager,
+    IHttpContextAccessor httpContextAccessor,
+    IJwtTokenGenerator jwtTokenGenerator) : IRequestHandler<ChangeCompanyCommand, IDomainResult<LoginCommandResponse>>
+{
+    public async Task<IDomainResult<LoginCommandResponse>> Handle(ChangeCompanyCommand request, CancellationToken cancellationToken)
+    {
+        if (httpContextAccessor.HttpContext is null) return DomainResult.NotFound<LoginCommandResponse>("Bu işlemi yapmaya yetkiniz yok");
+
+        String? userId = httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? string.Empty;
+        if (string.IsNullOrEmpty(userId)) return DomainResult.NotFound<LoginCommandResponse>("Bu işlemi yapmaya yetkiniz yok");
+
+        AppUser? user = await userManager.FindByIdAsync(userId);
+        if (user is null) return DomainResult.NotFound<LoginCommandResponse>("Kullanıcı bulunamadı");
+
+        List<CompanyUser> companyUsers = await companyUserRepository
+            .Where(c => c.AppUserId == user.Id)
+            .Include(c => c.Company)
+            .ToListAsync(cancellationToken);
+
+        List<Company> companies = companyUsers.Select(c => new Company
+        {
+            Id = c.CompanyId,
+            Name = c.Company!.Name,
+            TaxDepartment = c.Company.TaxDepartment,
+            TaxNumber = c.Company.TaxNumber,
+            FullAddress = c.Company.FullAddress
+        }).ToList();
+
+        LoginCommandResponse response = await jwtTokenGenerator.CreateToken(user, request.CompanyId, companies);
+        return DomainResult.Success(response);
+    }
+}
