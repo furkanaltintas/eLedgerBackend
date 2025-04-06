e6583257615b28c0e60007ada6be75be6533bf43
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-07 02:42:39 +0300

    update-user

---
+++ Application/Features/Users/UpdateUser/UpdateUserCommand.cs
@@ -0,0 +1,13 @@
+﻿using DomainResults.Common;
+using MediatR;
+
+namespace Application.Features.Users.UpdateUser;
+
+public record UpdateUserCommand(
+    Guid Id,
+    string FirstName,
+    string LastName,
+    string UserName,
+    string Email,
+    string? Password,
+    List<string> CompanyIds) : IRequest<IDomainResult<string>>;
---
+++ Application/Features/Users/UpdateUser/UpdateUserCommandHandler.cs
@@ -0,0 +1,69 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using DomainResults.Common;
+using GenericRepository;
+using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Users.UpdateUser;
+
+class UpdateUserCommandHandler(
+    UserManager<AppUser> userManager,
+    ICompanyUserRepository companyUserRepository,
+    IUnitOfWork unitOfWork,
+    IMapper mapper) : IRequestHandler<UpdateUserCommand, IDomainResult<string>>
+{
+    public async Task<IDomainResult<string>> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+    {
+        AppUser? user = await userManager.Users
+            .Where(u => u.Id == request.Id)
+            .Include(u => u.CompanyUsers)
+            .FirstOrDefaultAsync(cancellationToken);
+        if (user is null) return DomainResult<string>.Failed("User not found");
+
+        if (user.UserName != request.UserName)
+        {
+            Boolean isUserNameExists = await userManager.Users.AnyAsync(u => u.UserName == request.UserName, cancellationToken);
+            if (isUserNameExists) return DomainResult<string>.Failed("User already exists");
+        }
+
+        if (user.Email != request.Email)
+        {
+            Boolean isEmailExists = await userManager.Users.AnyAsync(u => u.Email == request.Email, cancellationToken);
+            if (isEmailExists) return DomainResult<string>.Failed("Email already exists");
+
+            user.EmailConfirmed = false;
+        }
+
+        mapper.Map(request, user);
+
+        IdentityResult identityResult = await userManager.UpdateAsync(user);
+        if (!identityResult.Succeeded) return DomainResult<string>.Failed(identityResult.Errors.Select(e => e.Description).ToList());
+
+        if (request.Password is not null)
+        {
+            String token = await userManager.GeneratePasswordResetTokenAsync(user);
+
+            identityResult = await userManager.ResetPasswordAsync(user, token, request.Password);
+            if (!identityResult.Succeeded) return DomainResult<string>.Failed(identityResult.Errors.Select(e => e.Description).ToList());
+        }
+
+        if (request.CompanyIds!.Any())
+        {
+            companyUserRepository.DeleteRange(user.CompanyUsers);
+
+            List<CompanyUser> companyUsers = request.CompanyIds!.Select(companyId => new CompanyUser
+            {
+                CompanyId = Guid.Parse(companyId),
+                AppUserId = user.Id
+            }).ToList();
+
+            await companyUserRepository.AddRangeAsync(companyUsers);
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+        }
+
+        return DomainResult.Success("User updated successfully");
+    }
+}
