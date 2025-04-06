de3913f05d4d19079bd140a5cf376fba8a126997
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-07 02:42:19 +0300

    create-user

---
+++ Application/Features/Users/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using DomainResults.Common;
+using MediatR;
+
+namespace Application.Features.Users.CreateUser;
+
+public record CreateUserCommand(
+    string FirstName,
+    string LastName,
+    string UserName,
+    string Email,
+    string Password,
+    List<string> CompanyIds) : IRequest<IDomainResult<string>>;
---
+++ Application/Features/Users/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using DomainResults.Common;
+using GenericRepository;
+using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Application.Features.Users.CreateUser;
+
+class CreateUserCommandHandler(
+    UserManager<AppUser> userManager,
+    ICompanyUserRepository companyUserRepository,
+    IUnitOfWork unitOfWork,
+    IMapper mapper) : IRequestHandler<CreateUserCommand, IDomainResult<string>>
+{
+    public async Task<IDomainResult<string>> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        Boolean isUserExists = await userManager.Users.AnyAsync(u => u.UserName == request.UserName || u.Email == request.Email, cancellationToken);
+        if (isUserExists) return DomainResult<string>.Failed("User already exists");
+
+        AppUser user = mapper.Map<AppUser>(request);
+
+        IdentityResult identityResult = await userManager.CreateAsync(user, request.Password);
+        if (!identityResult.Succeeded) return DomainResult<string>.Failed(identityResult.Errors.Select(e => e.Description).ToList());
+
+        await CompanyUsersAsync(request, user, cancellationToken);
+
+        return DomainResult.Success("Kullanıcı başarıyla oluşturuldu");
+    }
+
+    private async Task CompanyUsersAsync(CreateUserCommand request, AppUser user, CancellationToken cancellationToken)
+    {
+        List<CompanyUser> companyUsers = request.CompanyIds.Select(companyId => new CompanyUser
+        {
+            CompanyId = Guid.Parse(companyId),
+            AppUserId = user.Id
+        }).ToList();
+
+        await companyUserRepository.AddRangeAsync(companyUsers);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+    }
+}
