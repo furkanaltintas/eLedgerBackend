0e2fa2f3154915cc934e526778e1adc25fc2809e
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-07 02:43:16 +0300

    migrate-all-companies

---
+++ Application/Features/Companies/MigrateAllCompanies/MigrateAllCompaniesCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using Application.Services;
+using Domain.Entities;
+using Domain.Interfaces;
+using DomainResults.Common;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Companies.MigrateAllCompanies;
+
+class MigrateAllCompaniesCommandHandler(
+    ICompanyRepository companyRepository,
+    ICompanyService companyService) : IRequestHandler<MigrateAllCompaniesCommand, IDomainResult<string>>
+{
+    public async Task<IDomainResult<string>> Handle(MigrateAllCompaniesCommand request, CancellationToken cancellationToken)
+    {
+        List<Company> companies = await companyRepository.GetAll().ToListAsync(cancellationToken);
+
+        companyService.MigrateAllCompanies(companies);
+        return DomainResult.Success("Şirket databaseleri başarıyla güncellendi");
+    }
+}
