02e64f22371e8429f3f075bb80fc6ecb93151fd0
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-09 00:31:52 +0300

    CompanyCookie

---
+++ Application/Helpers/CompanyContextHelper.cs
@@ -0,0 +1,36 @@
+﻿using Application.Interfaces;
+using Infrastructure.Services.Cache;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Helpers;
+
+internal class CompanyContextHelper(
+    IHttpContextAccessor httpContextAccessor,
+    ICacheService cacheService) : ICompanyContextHelper
+{
+    public T GetCompanyFromContext<T>(string name)
+    {
+        String cacheKey = $"Company_{GetCompanyId()}-{name}";
+        T? result = cacheService.Get<T>(cacheKey);
+        if (result is null) return default!;
+        return result;
+    }
+
+    public void RemoveCompanyFromContext(string name)
+    {
+        cacheService.Remove($"Company_{GetCompanyId()}-{name}");
+    }
+
+    public void SetCompanyInContext<T>(string name, T value)
+    {
+        cacheService.Set($"Company_{GetCompanyId()}-{name}", value, TimeSpan.FromHours(1));
+    }
+
+    private string? GetCompanyId()
+    {
+        // HttpContext üzerinden CompanyId'yi alıyoruz
+        String? companyId = httpContextAccessor.HttpContext?.User.FindFirst("CompanyId")?.Value;
+        if (string.IsNullOrEmpty(companyId)) throw new Exception("CompanyId bulunamadı");
+        return companyId;
+    }
+}
---
+++ Application/Interfaces/ICompanyContextHelper.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Interfaces;
+
+public interface ICompanyContextHelper
+{
+    T GetCompanyFromContext<T>(string name);
+    void SetCompanyInContext<T>(string name, T value);
+    void RemoveCompanyFromContext(string name);
+}
