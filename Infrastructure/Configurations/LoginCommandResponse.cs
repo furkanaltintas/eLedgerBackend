a6bc9b0bbe33373311b0d8913e565263f2ba0491
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-14 04:26:47 +0300

    Referanslar değiştirildi

--- Infrastructure/Configurations/LoginCommandResponse.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace Infrastructure.Configurations;
-
-public record LoginCommandResponse(string Token, string RefreshToken, DateTime RefreshTokenExpires);
--- Infrastructure/InfrastructureServiceRegistration.cs
+++ Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,10 +1,13 @@
-﻿using Infrastructure.Services.Cache;
+﻿using Application.Common.Interfaces;
+using Infrastructure.Services.Cache;
+using Infrastructure.Services.Context;
 using Infrastructure.Services.Jwt;
+using Infrastructure.SignalR.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure;
 
-public static class DependencyInjection
+public static class InfrastructureServiceRegistration
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
@@ -14,6 +17,9 @@ public static class DependencyInjection
         services.AddMemoryCache();
         services.AddScoped<ICacheService, MemoryCacheService>();
         services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddScoped<ICompanyContextHelper, CompanyContextHelper>();
+        services.AddScoped<ISignalRService, SignalRService>();
+        services.AddSignalR();
 
         return services;
     }
