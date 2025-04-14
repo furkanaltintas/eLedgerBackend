0733932904c440c3c7988a3b73bf82b2e64cb900
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-14 04:26:32 +0300

    SignalR eklendi

---
+++ Infrastructure/SignalR/Hubs/ReportHub.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Infrastructure.SignalR.Hubs;
+
+public class ReportHub : Hub
+{
+}
---
+++ Infrastructure/SignalR/Services/SignalRService.cs
@@ -0,0 +1,14 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure.SignalR.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Infrastructure.SignalR.Services;
+
+public class SignalRService(IHubContext<ReportHub> hubContext) : ISignalRService
+{
+    public async Task SendPurchaseReportAsync(object data) =>
+        await hubContext.Clients.All.SendAsync("PurchaseReports", data);
+
+    public async Task SendDeleteReportAsync(object data) =>
+        await hubContext.Clients.All.SendAsync("PurchaseDeleteReport", data);
+}
