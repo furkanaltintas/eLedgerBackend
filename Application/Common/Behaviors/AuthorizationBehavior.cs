f68af29ded953329f76d11f663c439559507d989
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-14 04:28:40 +0300

    AuthorizationBehavior

---
+++ Application/Common/Behaviors/AuthorizationBehavior.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using System.Reflection;
+using System.Security.Claims;
+
+namespace Application.Common.Behaviors;
+
+public class AuthorizationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public AuthorizationBehavior(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var authorizeAttribute = request.GetType().GetCustomAttribute<Application.Security.AuthorizeAttribute>();
+        if(authorizeAttribute is not null)
+        {
+            ClaimsPrincipal? user = _httpContextAccessor.HttpContext?.User;
+            if(user is null || !authorizeAttribute.Roles.Any(role => user.IsInRole(role)))
+                throw new UnauthorizedAccessException("Yetkiniz yok.");
+        }
+
+        return await next();
+    }
+}
