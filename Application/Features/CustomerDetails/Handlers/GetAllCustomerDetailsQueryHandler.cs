14f3584e3155d68e6352d6efc549e4e88f02e393
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-17 01:34:42 +0300

    Revize işlemi yapıldı

--- Application/Features/CustomerDetails/Handlers/GetAllCustomerDetailsQueryHandler.cs
+++ Application/Features/CustomerDetails/Handlers/GetAllCustomerDetailsQueryHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Features.CustomerDetails.Queries;
-using Domain.Entities;
+using Application.Features.Customers.Constants;
+using Domain.Entities.Companies;
 using Domain.Interfaces;
 using DomainResults.Common;
 using MediatR;
@@ -7,12 +8,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Application.Features.CustomerDetails.Handlers;
 
-class GetAllCustomerDetailsQueryHandler(ICustomerRepository customerRepository) : IRequestHandler<GetAllCustomerDetailsQuery, IDomainResult<Customer>>
+internal sealed class GetAllCustomerDetailsQueryHandler(ICustomerRepository customerRepository) : IRequestHandler<GetAllCustomerDetailsQuery, IDomainResult<Customer>>
 {
     public async Task<IDomainResult<Customer>> Handle(GetAllCustomerDetailsQuery request, CancellationToken cancellationToken)
     {
-        Customer? customer = await customerRepository.Where(c => c.Id == request.CustomerId).Include(c => c.Details).FirstOrDefaultAsync(cancellationToken);
-        if (customer == null) return DomainResult.NotFound<Customer>("Customer not found");
+        Customer? customer = await customerRepository.Where(c => c.Id == request.CustomerId)
+                                                     .Include(c => c.Details)
+                                                     .FirstOrDefaultAsync(cancellationToken);
+        
+        if (customer is null) return DomainResult.NotFound<Customer>(CustomersMessages.NotFound);
         return DomainResult.Success(customer);
     }
 }
